fba6f2db3d9b9f19642d9a34823338a9c168ec24
Christoph Klein <christoph.klein@ziti.uni-heidelberg.de>
2019-05-15 11:43:43 +0200

    Separated regular expressions into source file

--- mcollector-modules/kav.rb
+++ mcollector-modules/kav.rb
@@ -1,46 +1,5 @@
 require_relative './base.rb'
-
-#########
-# REGEX #
-#########
-# the following variables should be global to test them when
-# including this file from another ruby script
-
-$expReg = /[eE][-+]?[[:digit:]]+/ # e.g. e+64 or E-0674
-$numReg = /[-+]?[[:digit:]]+(?:\.[[:digit:]]+)?#{$expReg}?/
-
-# The unit regex should allow a lot of symbols, e.g. GB/s, foo^4
-# It matches everything except space.
-$unitMaxSize        = 10 # maximum size of a unit, e.g. `Byte` has 4
-$unitReg          = /\S{1,#{$unitMaxSize}}/
-
-# regex of assignment symbols; the order is crucial; do not change it
-$linkReg = /-+>|=+>|=|:/ # divided by logical OR
-
-# numerical value
-$quantityReg = /(?<value>#{$numReg})#{$unitReg}?/
-
-# quoted value
-$quotationReg = /(?<value>"[^"]+")/
-
-# simple word value
-# which must consists of word characters: [a-zA-Z0-9_-]
-# the minus is contained, as we want to support detection of
-# words like `mp-data.ziti.uni-heidelberg.de`
-$wordReg = /(?<value>[\w-]+)/
-
-# date value
-# We need a date regex to add it before the quantity regex.
-# It is not possible to capture dates with the word regex, as
-# the word regex would also capture expressions like e.g.
-# `1654MB`, which should generally be captured by the quantity
-# regex to extract the number from the pattern.
-$dateReg = /(?<value>[0-9]{4}-[0-9]{2}-[0-9]{2})/
-
-# general value regex
-# the regular expressions here are processed from left to
-# right with decreasing priority.
-$valReg = /#{$quotationReg}|#{$dateReg}|#{$quantityReg}|#{$wordReg}/
+require_relative '../regular-expressions.rb'
 
 # This function returns a regex which matches any KEYWORD SPACE LINK
 # SPACE VALUE constellations. Moreover the VALUE and KEYWORD are
@@ -48,15 +7,20 @@ $valReg = /#{$quotationReg}|#{$dateReg}|#{$quantityReg}|#{$wordReg}/
 # If the keyword is omitted, it is assumed that the keyword can consist of
 # [_-[:alnum:]], which are numbers, alphabetic characters, underscore and
 # minus.
-def getKeyValueReg(keyword=nil)
-	if keyword != nil
+def getKeyValueReg(args = {})
+	args = {
+		:keyword => nil,
+		:allow_weird_keywords => false
+	}.merge(args)
+
+	if args[:keyword] != nil
 		# If the keyword is given everything in the keyword should be matched
 		# literally, thus we escape every special regex character
 		safe_keyword = Regexp.quote(keyword)
 		# We must us [[:blank:]] instead of \s, because \s includes \n!
 		/(?<keyword>#{safe_keyword})[[:blank:]]*#{$linkReg}[[:blank:]]*#{$valReg}/
 	else
-		if $options.wkeywords           # '+?' = non greedy '+'
+		if args[:allow_weird_keywords] # '+?' = non greedy '+'
 			/(?<keyword>[^,]+?)[[:blank:]]*#{$linkReg}[[:blank:]]*#{$valReg}/
 		else                       # '+?' = non greedy '+'
 			/(?<keyword>[_\-[:alnum:]]+?)[[:blank:]]*#{$linkReg}[[:blank:]]*#{$valReg}/
@@ -93,7 +57,7 @@ module MCollectorModule
 			pruned_str = input_str.clone
 
 			args[:keywords].each do |key|
-				md = pruned_str.match(getKeyValueReg(key))
+				md = pruned_str.match(getKeyValueReg(:keyword => key))
 				if !md.nil?
 					val = md["value"]
 					pruned_str = md.pre_match + md.post_match # remove match from str
---
+++ regular-expressions.rb
@@ -0,0 +1,41 @@
+#########
+# REGEX #
+#########
+# Collection of useful regular expressions
+
+$expReg = /[eE][-+]?[[:digit:]]+/ # e.g. e+64 or E-0674
+$numReg = /[-+]?[[:digit:]]+(?:\.[[:digit:]]+)?#{$expReg}?/
+
+# The unit regex should allow a lot of symbols, e.g. GB/s, foo^4
+# It matches everything except space.
+$unitMaxSize        = 10 # maximum size of a unit, e.g. `Byte` has 4
+$unitReg          = /\S{1,#{$unitMaxSize}}/
+
+# regex of assignment symbols; the order is crucial; do not change it
+$linkReg = /-+>|=+>|=|:/ # divided by logical OR
+
+# numerical value
+$quantityReg = /(?<value>#{$numReg})#{$unitReg}?/
+
+# quoted value
+$quotationReg = /(?<value>"[^"]+")/
+
+# simple word value
+# which must consists of word characters: [a-zA-Z0-9_-]
+# the minus is contained, as we want to support detection of
+# words like `mp-data.ziti.uni-heidelberg.de`
+$wordReg = /(?<value>[\w-]+)/
+
+# date value
+# We need a date regex to add it before the quantity regex.
+# It is not possible to capture dates with the word regex, as
+# the word regex would also capture expressions like e.g.
+# `1654MB`, which should generally be captured by the quantity
+# regex to extract the number from the pattern.
+$dateReg = /(?<value>[0-9]{4}-[0-9]{2}-[0-9]{2})/
+
+# general value regex
+# the regular expressions here are processed from left to
+# right with decreasing priority.
+$valReg = /#{$quotationReg}|#{$dateReg}|#{$quantityReg}|#{$wordReg}/
+
